b4b093cfa2c1cc1b640299334ad9c4aedc0c40ee
Bermely <141789480+Bermely@users.noreply.github.com>
2023-10-02 18:49:29 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    // Select the element with the ID 'button'
+    const buttonElement = document.getElementById('button');
+  
+    // Check if the button element exists
+    if (buttonElement) {
+      // Bind a click event listener to the button element
+      buttonElement.addEventListener('click', function() {
+        // This callback function will execute when the button is clicked
+        alert('Button clicked!');
+      });
+    }
+  }
